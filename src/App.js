e66d8692429084c5c806c17d1ee3f995c049628a
pappuparvez <pappuparvez@outlook.com>
2021-03-18 01:49:48 -0700

    bpl

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header/Header';
+import SelectPlayer from './Components/SelectPlayer/SelectPlayer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <SelectPlayer></SelectPlayer>
+     
+    </div>
+  );
+}
+
+export default App;
